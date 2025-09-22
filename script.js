4e068f7e27d93aea8d1a2ec31aba4bbaf990a92f
mmed <mila.medic2000@gmail.com>
2025-09-22 13:08:31 +1000

    body completed

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+const typeWriterController = {}
+
 /**
- * Function that types out text from a certain element giving the typewriter effect
+ * Function that types out text using the typewriter effect
  */
 async function typeWriterEffect(file, container) {
 
@@ -8,6 +10,18 @@ async function typeWriterEffect(file, container) {
       console.error("Container not found: ", container);
       return;
     }
+
+    if(typeWriterController[container]) {
+      clearTimeout(typeWriterController[container].timeoutId); // cancel timeout of the previous function execution
+      typeWriterController[container].cancelled = true;
+    }
+
+    const controller = { // controller of the current typewriting execution 
+      cancelled: false,
+      timeoutId: null
+    };
+
+    typeWriterController[container] = controller;
 
     element.textContent = "";
     try{
@@ -26,10 +40,13 @@ async function typeWriterEffect(file, container) {
       const speed = 5;
       let i = 0;
       function typeWriter() {
+        if(controller.cancelled) { // if execution is cancelled
+          return;
+        }
         if (i < fullText.length) {
           element.textContent += fullText.charAt(i); // fill the container with text one character at a time
           i++;
-          setTimeout(typeWriter, speed);
+          controller.timeoutId = setTimeout(typeWriter, speed);
         }
       }
 
@@ -43,8 +60,58 @@ async function typeWriterEffect(file, container) {
 
 
 
-/** FUNCTION CALLS */
 
-// document.addEventListener("DOMContentLoaded", typeWriterEffect);
+/** Load html from a file and place it inside a container */
+function loadText(file, container) {
+  
+  fetch(file) 
+  .then(response => {
+    if(!response.ok) {
+      throw new Error("Failed to load file: " + response.status);
+    }
+    return response.text(); // html text from the file
+  })
+  .then(html => {
+    const element = document.getElementById(container);
+    if (!element) {
+      console.error("Container not found: ", container);
+      return;
+    }
+    element.innerHTML = html;
+  })
+  .catch(error => {
+    console.error("Error loading html file: ", error);
+  });
+
+
+}
+
+
+/**
+ * Stop typewriting effect when other buttons are clicked
+ */
+document.querySelectorAll('#education-button', '#projects-button').forEach( button =>{
+  button.addEventListener('click', () => {
+    Object.values(typeWriterController).forEach(controller => {
+      controller.cancelled = true;
+      if(controller.timeoutId) {
+        clearTimeout(controller.timeoutId);
+      }
+    })
+    
+  })
+})
+
+
+/** 
+ * On-load function
+ */
+
+document.addEventListener(
+  "DOMContentLoaded", () => {
+    typeWriterEffect("about.html", "#main-content");
+  }
+
+);
 
 
